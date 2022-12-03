fcb879645090fcf13edf4f5f28387f123624fc6b
Rafa <57460462+rfpcmaia@users.noreply.github.com>
2022-12-03 15:55:14 -0500

    last commit before starting changes

--- Warzone.cpp
+++ Warzone.cpp
@@ -1,24 +1,23 @@
 // Warzone.cpp : This file contains the 'main' function. Program execution begins and ends there.
 
-#include "Map/MapDriver.h"
-#include "Player/PlayerDriver.h"
-#include "Orders/OrdersDriver.h"
-#include "Cards/CardsDriver.h"
-#include "GameEngine/GameEngineDriver.h"
-#include "CommandProcessing/CommandProcessingDriver.h"
-#include "LoggingObserver/LoggingObserverDriver.h"
+#include "Header/Map/MapDriver.h"
+#include "Header/GameEngine/GameEngineDriver.h"
+#include "Header/CommandProcessing/CommandProcessingDriver.h"
+#include "Header/LoggingObserver/LoggingObserverDriver.h"
+#include "Header/PlayerStrategies/PlayerStrategiesDriver.h"
 #include <iostream>
 
 int main()
 {
-    //P1
-    testCommandProcessor();
-    //P2
-    //testStartupPhase();
-    //P3
-    //testMainGameLoop();
-    //P4
-    //testOrderExecution();
-    //P5
-    //testLoggingObserver();
+    testPlayerStrategies();
+    // P1
+    // testCommandProcessor();
+    // P2
+    // testStartupPhase();
+    // P3
+    // testMainGameLoop();
+    // P4
+    // testOrderExecution();
+    // P5
+    // testLoggingObserver();
 }
